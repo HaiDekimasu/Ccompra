4b8f7c9fa014336835732f4ca66ea8f7e9b43c79
Haidekimasu <cadevillajefferson@gmail.com>
2024-07-02 23:24:27 -0400

    modified:   CarroCompras/CarroCompras/settings.py 	modified:   CarroCompras/carritoapp/models.py

--- CarroCompras/carritoapp/models.py
+++ CarroCompras/carritoapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+    
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f'{self.quantity} x {self.product.name}'
